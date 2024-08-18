5a482d948fea6a9ec86362abc2810c1b2a3ff3dd
Sandriely F M Toledo GTADSI33A <109091362+Sandriely1999@users.noreply.github.com>
2024-08-18 19:45:02 -0300

    Initial commit

---
+++ src/test/java/br/unasp/MvpSistemaDeDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unasp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpSistemaDeDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
